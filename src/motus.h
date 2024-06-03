7bcf881e937435b74a63cbe35e1f04b1c17efd07
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-03 20:16:54 +0200

    Update motus.h

--- src/motus.h
+++ src/motus.h
@@ -1,8 +1,8 @@
 #pragma once
 
 //Rôle : récupére une liste d'entiers et réalise l'affichage pour le joueur
-extern void affichache(const int[], const char[]);
+extern void affichage(int, int[], const char[]);
 //Rôle : gestoin de la  partie
 extern void Partie();
 //Rôle : vérifie l'état de la partie et la stop si besoin
-extern int finPartie(int,const int[]);
+extern int finPartie(int, int[]);
