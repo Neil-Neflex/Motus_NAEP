4898224dfb00151184130d9a4205724e55daf5cb
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-03 20:16:15 +0200

    Update mot.h

--- src/mot.h
+++ src/mot.h
@@ -1,21 +1,24 @@
 #pragma once
+#include <stdbool.h>
 
 //Définition d'un nouveau type 'Mot'
-typedef struct { 
+typedef struct {
     int size;       //size spécifie la taille du mot
-    char mot[];     //mot est une liste de caractère qui contient le mot
+    char *mot;     //mot est une liste de caractère qui contient le mot
 }Mot;
 
 //crée un nouvel élément de type Mot
-extern Mot initMot(); 
+extern Mot initMot();
 // Rôle : compare longueure du mot entré en param avec la valeur attendue
-extern int longueurMot(Mot,const int);
+extern bool longueurMot(char *, char *);
 // Rôle : renvoie vrai si pile est vide faux sinon
 extern int presence(Mot,const char);
 // Rôle : renvoie 1 si l'élément à la position entrée en param est bien la même pour
 // le mot et l'entrée du joueur
-extern int position (int, Mot, const char[]);
+extern void position(char *, const char *, char *);
 // Rôle : vérifie le mot entrée par le joueur par rapport au mot à trouver
-extern char * verifMot(Mot, const char[]);
+extern int verifMot(char *, const char *, char *);
 //
-extern int positionLettres();
+extern void positionLettres(char, const char *, char *, int);
+
+extern void affichageMot(const char *, const char *);
