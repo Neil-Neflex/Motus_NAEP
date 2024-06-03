024bb61114f6733b355f7358922f9fa09c2b83fe
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-03 20:17:24 +0200

    Update dico.c

--- src/dico.c
+++ src/dico.c
@@ -1,31 +1,29 @@
-/**
+/*
  * Ce fichier s'occupe de la gestion des fichiers et de la gestion relative au dictionnaire
  * Il n'intervient qu'au début de l'exécution du programme
  *
- * @authors N. Amrane & E. Patijunas     neil.amrane@etu.univ-cotedazur.fr 
+ * @authors N. Amrane & E. Patijunas     neil.amrane@etu.univ-cotedazur.fr
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-
 #include "dico.h"
 
 FILE* dico = NULL; //Ce pointeur de fichier contiendra notre dictionnaire
 int NBMOTS = 0;
 const char PATH[] =  "../ressources/DicoMotus"; //chemin vers le dictionnaire
 
-
 //Cette procédure a pour but l'ouverture et la gestion du dictionnaire
 int Gestion(void){
     int cLu;
-    dico = fopen("../ressources/DicoMotus", "r"); //on ouvre dico.txt en lecture seule
+    dico = fopen("C:/Users/edvin/CLionProjects/ProjetMotus/DicoMotus", "r"); //on ouvre dico.txt en lecture seule
     //On test si le dictionnaire s'est bien ouvert
     if (dico == NULL){
-        printf("Ouverture du dictionnaire impossilbe\n");
+        printf("Ouverture du dictionnaire impossible\n");
         return 0; //on retourne 0 s'il y a une erreur
     }
-    
+
     printf("Initialisation du fichier achevée\n");
     //On parcours le dictionnaire pour compter le nombre de mots
     do{
@@ -37,12 +35,12 @@ int Gestion(void){
 
 //cette fonction retourne une valeur pseudo-aléatoire entre à et le paramètre nbMax
 int aleatoire(int nbMax){
-    srand(time(NULL));      //initialisation du générateur pseudo-aléatioire 
+    srand(time(NULL));      //initialisation du générateur pseudo-aléatioire
                             //sur l'horloge de l'ordinateur
     return (rand() % nbMax);     //retour d'une valeur comprise entre 0 et nbMax
 }
 
-//Cette procédure à pour but le choix du mot dans le ditctionnaire 
+//Cette procédure à pour but le choix du mot dans le dictionnaire
 int choixMot(char *adress){
     int cLu;
     int numMot; //numMot contiendra la ligne du mot choisi aléatoirement
@@ -64,18 +62,9 @@ int choixMot(char *adress){
     }
     fgets(adress, 50, dico); //appel à la procédure fgets pour lire la ligne choisie
                             //le mot lu est stocker à l'adresse du pointeur
-    
+
     adress[strlen(adress)-1] = '\0'; //on retire le caractère '\0' et on le remplace par '\0'
 
     fclose(dico); //on referme le dictionnaire
     return 1; // on retourne 1 pour confirmer la fin et bonne exécution du programme
-}
-
-
-int main(int argc, char *argv[]){
-    Mot motSecret;
-    char *p = malloc(sizeof(Mot));
-    Gestion();
-    choixMot(p);
-    printf(p);
 }
