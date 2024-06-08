4a22c9b79c74a3b72dc80f57b5858a6a4f78b9d3
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-08 14:21:17 +0200

    Update display.h

--- src/display.h
+++ src/display.h
@@ -1,8 +1,7 @@
-/**
-* Ce programme est un jeu de réflexion basé sur les connaissances de la langue française
- * qu'a le joueur. Le programmme présente la première lettre d'un mot au joueur et ce
- * dernier a 6 essaies pour trouver le mot. Pour l'aider le programme indique au joueur
- * si les lettres du mot entré sont bien positionnées ou non
- *
- * @authors N. Amrane & E. Patijunas     neil.amrane@etu.univ-cotedazur.fr
- */
+#pragma once
+
+extern void init_display(int, char *);
+extern void box_orangeDef(char, char l);
+extern void box_bleuDef(char, char l);
+extern void box_rougeDef(char, char l);
+extern void GenereBoxDisp(char, int, int);
