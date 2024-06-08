f59357cb4f1c8d7ab17794ded55676cbf77b2bde
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-08 11:42:55 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/**
+ * Creation date: 17−Apr−2024 16:18
+ * Last file update: 05−Jun−2024 19:11
+ * @authors N. Amrane & E. Patiejunas     neil.amrane@etu.univ-cotedazur.fr     edvinas.patiejunas@etu.univ-cotedazur.fr
+ *
+ * Le rôle de main.c est simplement de démarrer le jeu et de gérer le flux de contrôle au niveau le plus élevé.
+ */
+
+
+#include "motus.h"
+
+int main(void) {
+    Partie();    // Démarrer le jeu
+    return 0;    // Retourner 0 si le programme se termine normalement
+}
