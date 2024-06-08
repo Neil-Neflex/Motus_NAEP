b567ed975be9ac4b13fa950a3415b1ca5c3a2ad2
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-08 14:20:38 +0200

    Update callback.h

--- src/callback.h
+++ src/callback.h
@@ -1,8 +1,5 @@
-/**
-* Ce programme est un jeu de réflexion basé sur les connaissances de la langue française
- * qu'a le joueur. Le programmme présente la première lettre d'un mot au joueur et ce
- * dernier a 6 essaies pour trouver le mot. Pour l'aider le programme indique au joueur
- * si les lettres du mot entré sont bien positionnées ou non
- *
- * @authors N. Amrane & E. Patijunas     neil.amrane@etu.univ-cotedazur.fr
- */
+#pragma once
+
+extern void stop_callback(Widget, void *);
+extern void restart_callback(Widget, void *);
+extern void test_callback(Widget, void *);
