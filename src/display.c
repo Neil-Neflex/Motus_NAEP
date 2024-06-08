62a5f4e3265238331686271ed831853b6d0d0770
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-08 21:42:49 +0200

    Update display.c

--- src/display.c
+++ src/display.c
@@ -1,61 +1,54 @@
-#include <libsx.h>
 #include <stdio.h>
-#include "callback.h"
+#include <stdlib.h>
+#include <libsx.h>
+#include "data.h"
+#include "callbacks.h"
 
-// Déclaration des widgets globaux pour les utiliser dans les callbacks
-Widget zone_text;
+#define TAILLEZONEAFFICHAGE 255
+#define WORD_LENGTH 8 // Assuming the length of the word is 8
+#define MAX_ATTEMPTS 5 // Assuming a maximum of 5 attempts
 
-// Callback pour redessiner une boîte colorée
-void RedrawColorBox(Widget w, int width, int height, void *data)
+Widget ZoneSaisie; // la zone de saisie
+Widget **WordLabels; // les labels pour afficher les mots
+Widget BQuit; // le bouton Quit pour terminer le programme
+
+/* Rôle: création et assemblage des widgets */
+void init_display(int argc, char *argv[], void *d)
 {
-    int color = *(int *)data;
-    SetFgColor(w, color);
-    DrawFilledBox(w, 0, 0, width, height);
-}
+    int i, j;
 
-// Initialisation de l'affichage
-void init_display(int argc, char *argv[])
-{
-    Widget test, restart, stop;
+    // créer les composants graphiques
+    ZoneSaisie = MakeStringEntry(NULL, TAILLEZONEAFFICHAGE, NULL, NULL);
+    BQuit = MakeButton("Quit", quit, NULL);
 
-    // Génération des boutons utilisateur
-    test = MakeButton("Tester", test_callback, NULL);  // Initialisation bouton tester
-    restart = MakeButton("Recommencer", restart_callback, NULL); // Initialisation bouton recommencer
-    stop = MakeButton("Arrêter", stop_callback, NULL); // Initialisation bouton arrêter
-
-    SetWidgetPos(test, PLACE_UNDER, NULL, NO_CARE, NULL); // Positionnement bouton tester
-    SetWidgetPos(restart, PLACE_RIGHT, test, NO_CARE, NULL); // Positionnement bouton recommencer
-    SetWidgetPos(stop, PLACE_RIGHT, restart, NO_CARE, NULL); // Positionnement bouton arrêter
-
-    // Génération de l'espace textuel
-    zone_text = MakeTextWidget(NULL, 0, TRUE, 20, 100); // Initialisation zone textuelle
-    SetWidgetPos(zone_text, PLACE_UNDER, test, NO_CARE, NULL); // Positionnement de la zone de texte
-}
-
-// Rôle : crée une boîte colorée avec la lettre à l'intérieur
-void create_color_box(char l, int color)
-{
-    Widget box;
-    box = MakeDrawArea(100, 100, RedrawColorBox, &color); // Boîte colorée
-    SetWidgetPos(box, PLACE_UNDER, NULL, NO_CARE, NULL);
-    char letter[2] = {l, '\0'};
-    DrawText(box, letter, 1, 50, 50); // Insertion de la lettre dans la case
-}
-
-// Rôle : génère les boîtes en fonction du résultat du test du mot que l'utilisateur a entré
-void GenereBoxDisp(char result[], int pos_x, int pos_y)
-{
-    for (int i = 0; i < strlen(result); i++) {
-        switch (result[i]) {
-            case 'O': // Lettre bien placée
-                create_color_box(result[i], GetRGBColor(255, 165, 0)); // Orange
-                break;
-            case 'B': // Lettre présente mais mal placée
-                create_color_box(result[i], GetRGBColor(0, 0, 255)); // Bleu
-                break;
-            case 'R': // Lettre absente
-                create_color_box(result[i], GetRGBColor(255, 0, 0)); // Rouge
-                break;
+    // Créer les labels pour afficher les mots
+    WordLabels = (Widget **)malloc(MAX_ATTEMPTS * sizeof(Widget *));
+    for (i = 0; i < MAX_ATTEMPTS; i++) {
+        WordLabels[i] = (Widget *)malloc(WORD_LENGTH * sizeof(Widget));
+        for (j = 0; j < WORD_LENGTH; j++) {
+            WordLabels[i][j] = MakeLabel(" ");
         }
     }
+
+    // Placement des composants graphiques dans la fenêtre
+    SetWidgetPos(ZoneSaisie, PLACE_UNDER, NULL, NO_CARE, NULL);
+    SetWidgetPos(BQuit, PLACE_UNDER, ZoneSaisie, NO_CARE, NULL);
+
+    for (i = 0; i < MAX_ATTEMPTS; i++) {
+        for (j = 0; j < WORD_LENGTH; j++) {
+            if (i == 0 && j == 0) {
+                SetWidgetPos(WordLabels[i][j], PLACE_UNDER, BQuit, NO_CARE, NULL);
+            } else if (j == 0) {
+                SetWidgetPos(WordLabels[i][j], PLACE_UNDER, WordLabels[i - 1][0], NO_CARE, NULL);
+            } else {
+                SetWidgetPos(WordLabels[i][j], PLACE_RIGHT, WordLabels[i][j - 1], NO_CARE, NULL);
+            }
+        }
+    }
+
+    // pour gérer les couleurs
+    GetStandardColors();
+
+    // pour afficher l’interface
+    ShowDisplay();
 }
