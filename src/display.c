0b3ff0d09062fa68bbd3919723ad3d1c0364aedd
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-08 14:20:59 +0200

    Update display.c

--- src/display.c
+++ src/display.c
@@ -1,14 +1,3 @@
-/**
-* Ce programme est un jeu de réflexion basé sur les connaissances de la langue française
- * qu'a le joueur. Le programmme présente la première lettre d'un mot au joueur et ce
- * dernier a 6 essaies pour trouver le mot. Pour l'aider le programme indique au joueur
- * si les lettres du mot entré sont bien positionnées ou non
- * 
- * 
- *Ce fichier gère l'affichage de l'interface utilisateur
- *
- * @authors N. Amrane & E. Patijunas     neil.amrane@etu.univ-cotedazur.fr
- */
 #include <libsx.h>
 #include <stdio.h>
 #include "callback.h"
