20500f524b53d815412fbb03e7a48558f42817f2
Patiejunas Edvinas <119620232+pe106026@users.noreply.github.com>
2024-06-08 21:17:05 +0200

    Update callback.c

--- src/callback.c
+++ src/callback.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libsx.h>
+#include <string.h>
 #include "data.h"
 
 #define MAX_ROWS 6
-#define MAX_COLS 8
 #define WORD_LEN 8
 
-Widget tiles[MAX_ROWS][MAX_COLS];
 Widget input_text;
+Widget draw_area;
 char motMystere[WORD_LEN];
 char etat[WORD_LEN];
 int essais = 0;
@@ -16,6 +16,7 @@ int essais = 0;
 void create_interface(void);
 void quit_callback(Widget w, void *data);
 void check_word_callback(Widget w, void *data);
+void draw_callback(Widget w, int width, int height, void *data);
 
 int main(int argc, char *argv[])
 {
@@ -45,22 +46,13 @@ void create_interface(void)
     Widget quit_button = MakeButton("Quit", quit_callback, NULL);
     Widget check_button = MakeButton("Check", check_word_callback, NULL);
     input_text = MakeStringEntry(NULL, 20, NULL, NULL);
+    draw_area = MakeDrawArea(400, 300, draw_callback, NULL);
 
-    Widget button_box = MakeHorizBox(NULL);
-    Widget grid_box = MakeVertBox(NULL);
-
-    SetWidgetPos(button_box, PLACE_UNDER, grid_box, NULL);
-    SetWidgetPos(quit_button, PLACE_RIGHT, check_button, NULL);
-    SetWidgetPos(input_text, PLACE_UNDER, button_box, NULL);
-
-    for (int i = 0; i < MAX_ROWS; i++) {
-        Widget row_box = MakeHorizBox(grid_box);
-        for (int j = 0; j < WORD_LEN; j++) {
-            tiles[i][j] = MakeButton(" ", NULL, NULL);
-            SetBgColor(tiles[i][j], "blue");
-            SetWidgetPos(tiles[i][j], PLACE_RIGHT, NULL, NULL);
-        }
-    }
+    // Positionnement des boutons et de l'entrée de texte
+    SetWidgetPos(check_button, NO_CARE, NULL, NO_CARE, NULL);
+    SetWidgetPos(quit_button, PLACE_RIGHT, check_button, NO_CARE, NULL);
+    SetWidgetPos(input_text, PLACE_UNDER, check_button, NO_CARE, NULL);
+    SetWidgetPos(draw_area, PLACE_UNDER, input_text, NO_CARE, NULL);
 
     ShowDisplay();
 }
@@ -86,17 +78,9 @@ void check_word_callback(Widget w, void *data)
     }
 
     int gagne = verifMot(motPropose, motMystere, etat);
-    for (int i = 0; i < WORD_LEN; i++) {
-        if (etat[i] == 'x') {
-            SetBgColor(tiles[essais][i], "orange");
-        } else if (etat[i] == 'o') {
-            SetBgColor(tiles[essais][i], "blue");
-        } else {
-            SetBgColor(tiles[essais][i], "red");
-        }
-        SetLabel(tiles[essais][i], motPropose + i);
-    }
     essais++;
+
+    ClearDrawArea(draw_area);
 
     if (gagne) {
         printf("Félicitations ! Vous avez trouvé le mot %s.\n", motMystere);
@@ -104,5 +88,28 @@ void check_word_callback(Widget w, void *data)
     } else if (essais >= MAX_ROWS) {
         printf("Malheureusement, vous n'avez pas trouvé le mot. Le mot était %s.\n", motMystere);
         exit(0);
+    }
+
+    DrawText(draw_area, 10, essais * 30, motPropose, strlen(motPropose));
+}
+
+void draw_callback(Widget w, int width, int height, void *data)
+{
+    for (int i = 0; i < essais; i++) {
+        for (int j = 0; j < WORD_LEN; j++) {
+            char c = etat[j];
+            switch (c) {
+                case 'x':
+                    SetColor("red");
+                    break;
+                case 'o':
+                    SetColor("orange");
+                    break;
+                default:
+                    SetColor("blue");
+                    break;
+            }
+            DrawBox(draw_area, j * 30, i * 30, 25, 25);
+        }
     }
 }
